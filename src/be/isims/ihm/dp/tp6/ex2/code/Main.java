e6fc1ed2a704065ce3ac6bb5ebcf7747700c2a5e
Tee <nico.J.64@hotmail.be>
2020-05-16 17:07:11 +0200

    Plantuml removed

--- src/be/isims/ihm/dp/tp6/ex2/code/Main.java
+++
@@ -1,8 +0,0 @@
-package be.isims.ihm.dp.tp6.ex2.code;
-
-public class Main {
-    public static void main(String[] args) {
-        CopierSansBlanc b = new CopierSansBlanc();
-        b.copier("oui.txt", "ouicopie.txt");
-    }
-}
